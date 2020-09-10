32fdac7421775d2ff0be285d6d5d2db1dde8d50c
vaidyanathanms <msvaidyanathan1729@gmail.com>
2020-09-09 21:56:54 -0400

    major changes: now added capability for single chain with a given probbability distn. made a cimulative distn. YET TO DEBUG. next step is to make the distribution arrays in 2D for multiple chains

--- src_switchgrass/build_polymer/genconf.py
+++ src_switchgrass/build_polymer/genconf.py
@@ -6,18 +6,18 @@
 # An iterative procedure is used
 
 # References for lignin structure: 
-# (A) Samuel et al., Frontiers in Energy Research, 1 (14) 2014
-# (B) Yan et al., Biomass and Bioenergy 34, 48-53, 2010 
+# (A) Yan et al., Biomass and Bioenergy 34, 48-53, 2010 
+# (B) Samuel et al., Frontiers in Energy Research, 1 (14) 2014
+
 
 # Switchgrass variety: Alamo switchgrass
 
-# G:S = 0.78, H = 2 (A); H:G:S = 27:41:32 (B)
-# pCA:FA = 6:32 (A); pCA:FA = 1 (B)
+# H:G:S = 26:42:32 (A); G:S = 0.75 - 0.78, H = 2 (B)
+# pCA:FA = 1 (A); pCA:FA = 6:32 (B) 
 
 #------------------------------------------------------------------
 
 # Import modules
-
 import os
 import sys
 import numpy
@@ -26,80 +26,106 @@ import shutil
 import glob
 
 # Function definitions
-
-from make_genpsf import psfgen_headers
-from make_genpsf import monomer_ratios
-from make_genpsf import psfgen_postprocess
 from make_genpsf import gencpy
+from make_genpsf import psfgen_headers
+from make_genpsf import psfgen_postprocess
+from make_genpsf import residue_ratios
+from make_genpsf import linker_ratios
+from make_genpsf import init_logwrite
+from make_genpsf import cumul_probdist
+from make_genpsf import create_segments
+from make_genpsf import check_distribution
+from make_genpsf import make_segments
 
 # Input data
-
 casenum    = 1  # case number
 biomas_typ  = 'switchgrass_' # type of biomass; end with _
-deg_poly   = 18 # degree of polymerization (final)
-swit_opt   = 1 # references, A,B (add more and hard code if necesary)
+deg_poly   = 16 # degree of polymerization (final)
+swit_opt   = 'A' # references, A,B (add more and hard code if necesary)
 input_top = 'lignin.top' # topology file input
-input_pdb  = 'G-b04L-G.pdb' # file input - dimer
+input_pdb  = 'G-bO4L-G.pdb' # file input - dimer
+seg_name = 'swli' #name of segment: switchgrass lignin
+num_chains = 1 # number of chains
+tol = 0.1 # relative tolerance
 
 # Output file names (will be generated automatically)
-
 tcl_fname  = biomas_typ + str(casenum) + '.tcl' # outfile for tcl
 pdbpsf_fname = biomas_typ + str(casenum)  #prefix for pdb/psf files
+alllist_fname = 'alllist_' + str(casenum) + '.tcl' #all seg/link list
+log_fname = 'log_' + str(casenum) + '.txt' #log file
 
-# Define empty arrays and default values
-
-struct_arr = []
-gsrat   = 0 # G:S
-pfrat   = 0 # PCA:FA
-num_php = 0 # num of PHP (H) monomers
-num_gua = 0 # num of GUA (G) monomers
-num_syr = 0 # num of SYR (S) monomers
-num_pca = 0 # num of PCA monomers
-num_fa  = 0 # num of FA monomers
-
-
-# Throw exceptions
-
+# Read defaults and throw exceptions
 if not os.path.exists(input_top):
     exit('Topology file not found \n')
 
 if not os.path.exists(input_pdb):
     exit('Dimer file not found \n')
 
-optflag = monomer_ratios(swit_opt)
-if optflag == -1:
-    exit('ERROR: Unknown option for parameters \n')
+resperc_dict = residue_ratios(swit_opt) # residue % dictionary mode
+if not bool(resperc_dict):
+    exit('ERROR: Unknown option for monomer parameters \n')
+
+linkperc = linker_ratios(swit_opt) # linker % dictionary mode
+if not bool(linkperc):
+    exit('ERROR: Unknown option for linker parameters \n')
+
+#! check ordering
+for key, value in resperc_dict.items():
+    print(key,value)
+
+for key, value in resperc_dict.items():
+    print(key,value)
     
-
 # Get directory info
-
 srcdir = os.getcwd()
-outdir = cwd + str('/casenum_') + str(casenum) # out directory
+outdir = srcdir + str('/casenum_') + str(casenum) # out directory
 
 # Create main directory and copy required files
-
 if not os.path.isdir(outdir):
     os.mkdir(outdir)
-    
+
 gencpy(srcdir,destdir,input_top)
 gencpy(srcdir,destdir,input_pdb)
 
 # Open file and write headers
-
 fmain = open(outdir + '/' + tcl_fname,'w')
 psfgen_headers(fmain,input_top,pdbpsf_name)
-monomer_ratios(swit_opt)
-psfgen_postprocess(fmain,input_pdb)
-         
+flist = open(outdir + '/' + alllist_fname,'w')
+flist.write('; Contains all segments and linkers for NAMD files.\n')
+flog = open(outdir + '/' + alllist_fname,'w')
+init_logwrite(flog,casenum,biomass_typ,deg_poly,swit_opt,input_top\
+              ,input_pdb,seg_name,num_chains)
+print('Begin analysis for ',biomass_typ,', case_num: ', casenum)
+            
+# Number of iterations needed per chain
+niter = int(log(deg_poly)/log(2)) #closest power of 2
+flog.write('Number of iterations per chain: %d\n ' %(niter))
 
+# Create cumulative probability distribution of segments
+flog.write('Making cumulative distribution..\n')
+cumul_monarr = cumul_probdist(resperc_dict)
+print(cumul_monarr)
+    
+# Set default list and generate segments/linkers
+res_cumulprob_list = [] # add onto this to know the % mons of (n-1)th iteration
+res_cumulprob_list = create_segments(flist,nmons,segname,\ 
+                                     resperc_dict,cumul_monarr)
+distflag = check_distribution(resperc_dict,res_cumulprob_list,tol,flog,1)
 
-# Generate segments
-
-
+# Write segments according to iteration number
+for iterval in range(niter):
+    
+    iter_num  = itreval + 1
+    nmonsthisiter = pow(2,iter_num)
+    write_segments(fmain,iter_num,nmonsthisiter,segname,res_cumulprob_list)
+              
+        
+    
+    
 
 # Generate patches
 
 
 # Run NAMD
-
+psfgen_postprocess(fmain,input_pdb)
 
